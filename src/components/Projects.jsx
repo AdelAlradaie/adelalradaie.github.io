0db9d6b5abfffcb19003a770c7e4937c789f4311
AdelAlradaie <adelmohm209@gmail.com>
2025-01-10 02:08:15 +0300

    update projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,15 +9,15 @@ const Projects = () => {
     const [isPending, startTransition] = useTransition()
     const [index, setIndex] = useState(0)
     const goNext = () => {
-       startTransition(()=>{
-        setIndex((prev) => (prev + 1) % projects.length)
-       })
+        startTransition(() => {
+            setIndex((prev) => (prev + 1) % projects.length)
+        })
 
     }
     const goPrev = () => {
-       startTransition(()=>{
-        setIndex((prev) => (prev - 1 + projects.length) % projects.length)
-       })
+        startTransition(() => {
+            setIndex((prev) => (prev - 1 + projects.length) % projects.length)
+        })
     }
 
     useEffect(() => {
@@ -28,61 +28,69 @@ const Projects = () => {
     }, [])
 
     return (
-        <section className="bg-white py-20  ">
+        <section className="bg-[#0a2540] py-8 my-4  ">
             <div className="container mx-auto px-4">
-              <Fade bottom duration={2000} distance="20px">
-              <div className="flex items-center gap-2 ">
-                    <Laptop color="#00d4ff" size={30} />
-                    <h1 className="text-[2.5rem] mb-4 text-[#00d4ff]">Projects</h1>
-                </div>
-              </Fade>
+                <Fade bottom duration={2000} distance="20px">
+                    <div className="flex items-center gap-2 mb-6 ">
+                        <Laptop color="#00d4ff" size={30} />
+                        <h1 className="text-[2.5rem]  text-[#00d4ff]">Projects</h1>
+                    </div>
+                </Fade>
 
-                <Card className="py-4 border-2 bg-[#0a2540] max-w-[986px]  mx-auto w-full shadow-md">
-                    <CardContent className=" p-6  grid grid-cols-1 md:grid-cols-2 gap-4 ">
-                        <div className="relative h-[250px]  w-full  overflow-hidden">
-                            <img src={projects[index].images[0].link} alt="" className=" absolute h-full w-full rounded-t-lg  object-cover" />
-                            <ChevronRight color="#00d4ff" onClick={() => goNext()} size={35} className="absolute cursor-pointer  top-1/2 right-4 " />
-                            <ChevronLeft onClick={() => goPrev()} size={35} className="absolute  top-1/2 left-4  cursor-pointer" color="#00d4ff" />
-                        </div>
-                        <div className="w-full">
-                            <div className="grid gap-1 ">
-
-                                <p className="text-3xl text-[#00d4ff]">{projects[index].name}</p>
-                                <div className="flex items-center gap-2">
-
-                                    {projects[index].github && <a target="_blank" className="hover:bg-[#ffffff]  hover:shadow-lg hover:scale-110 transition-transform duration-300 p-1" rel="noopener noreferrer" href={`${projects[index].github}`}>
-                                        <Github size={25} color="#00d4ff" /></a>}
-                                    {projects[index].link && <a className="hover:bg-[#ffffff]  hover:shadow-lg hover:scale-110 transition-transform duration-300 p-1" target="_blank" rel="noopener noreferrer" href={`${projects[index].link}`}>
-                                        <EyeIcon size={25} color="#00d4ff" className="font-bold" /></a>}
+                <Fade bottom duration={2000} distance="20px">
+             <div className=" grid grid-cols-1   md:grid-cols-2  lg:grid-cols-4 gap-4">
+                    {projects.map((project, index) => (
+                         
+                        <div className="h-[350px] max-w-[450px] w-full   mx-auto group perspective cursor-pointer   ">
+                            <div className="relative h-full w-full group-hover:rotate-y-180  shadow-xl rounded duration-1000 preserve-3d ">
+                                <div className="absolute backface-hidden  h-full w-full">
+                                    <img className="h-full w-full object-center object-containe" src={project.images[0].link} alt="" />
                                 </div>
+                                <div className=" absolute h-full w-full rotate-y-180 backface-hidden p-4 border-2 border-[#00d4ff] bg-[#0a2540]">
+                                    <div className="grid gap-1 ">
 
-                                <p className="text-[#adbdcc] font-semibold">{projects[index].desc}</p>
+                                        <p className="text-3xl text-[#fff]">{project.name}</p>
+                                        <div className="flex items-center gap-2">
 
-                                <div className="mt-4 flex flex-col gap-2">
-                                    <h2 className="text-2xl text-[#00d4ff] ">Technologies</h2>
-                                    <div className="flex  items-center flex-wrap   gap-2">
-                                        {projects[index].technologies.map((tech) => (
-                                            <TooltipProvider>
-                                                <Tooltip>
-                                                    <TooltipTrigger>
-                                                        <div className="">
-                                                            {tech.Icon}
-                                                        </div>
-                                                    </TooltipTrigger>
-                                                    <TooltipContent className="bg-black text-white font-bold">
-                                                        {tech.title}
-                                                    </TooltipContent>
-                                                </Tooltip>
-                                            </TooltipProvider>
+                                            {project.github && <a target="_blank" className="hover:bg-[#ffffff]  hover:shadow-lg hover:scale-110 transition-transform duration-300 p-1" rel="noopener noreferrer" href={`${project.github}`}>
+                                                <Github size={25} color="#00d4ff" /></a>}
+                                            {project.link && <a className="hover:bg-[#ffffff]  hover:shadow-lg hover:scale-110 transition-transform duration-300 p-1" target="_blank" rel="noopener noreferrer" href={`${project.link}`}>
+                                                <EyeIcon size={25} color="#00d4ff" className="font-bold" /></a>}
+                                        </div>
 
-                                        ))}
+                                        <p className="text-[#adbdcc] font-semibold">{project.desc}</p>
 
+                                        <div className="mt-4 flex flex-col gap-2">
+                                            <h2 className="text-2xl text-[#fff] ">Technologies</h2>
+                                            <div className="flex  items-center flex-wrap   gap-2">
+                                                {project.technologies.map((tech) => (
+                                                    <TooltipProvider>
+                                                        <Tooltip>
+                                                            <TooltipTrigger>
+                                                                <div className="">
+                                                                    {tech.Icon}
+                                                                </div>
+                                                            </TooltipTrigger>
+                                                            <TooltipContent className="bg-black text-white font-bold">
+                                                                {tech.title}
+                                                            </TooltipContent>
+                                                        </Tooltip>
+                                                    </TooltipProvider>
+
+                                                ))}
+
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </CardContent>
-                </Card>
+                        </div>      
+                    ))}
+                </div>
+      
+                </Fade>
+
+             
 
 
             </div>

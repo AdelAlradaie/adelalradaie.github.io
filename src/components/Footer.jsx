b03adab6eab4a0869f9d9f1c00953b1dd4ca8aba
AdelAlradaie <adelmohm209@gmail.com>
2025-01-10 22:40:52 +0300

    added profile img

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,24 @@
 import SocialLinks from "@/common/socialLinks"
 import { FooterDetails } from "@/constants/portfolio"
-
+import { Avatar, AvatarImage } from "./ui/avatar"
+import profileImg from "../assets/images/profile.jpg"
 
 
 const Footer = () => {
     return (
         <footer className="bg-[#0a2540]  py-20">
-            <div className="container mx-auto px-4">
-                <div className="flex flex-col ">
-                    <h1 className="text-3xl text-white font-semibold mb-4">{FooterDetails.title}</h1>
+            <div className="container mx-auto px-4 flex flex-col-reverse lg:flex-row justify-between items-center">
+                <div className="flex flex-col mt-6 lg:mt-0 ">
+                    <h1 className="text-3xl text-white font-semibold mb-6">{FooterDetails.title}</h1>
                     <p className="text-[#adbdcc]">{FooterDetails.desc}</p>
-                    <p className="text-white my-6">Contact : <span className="text-[#00d4ff]">{FooterDetails.email}</span></p>
+                    <p className="text-white my-8">Contact : <span className="text-[#00d4ff]">{FooterDetails.email}</span></p>
 
                     <SocialLinks/>
+                </div>
+                <div>
+                    <Avatar className="max-w-[250px] max-h-[250px] h-full w-full  ">
+                        <AvatarImage  className="object-cover object-center"  src={profileImg}/>
+                    </Avatar>
                 </div>
             </div>
         </footer>

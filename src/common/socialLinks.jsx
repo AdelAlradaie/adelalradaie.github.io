0d68e9f7107c327ab78d87e42da4c7da36aafb34
AdelAlradaie <adelmohm209@gmail.com>
2025-01-10 22:38:26 +0300

    update links

--- src/common/socialLinks.jsx
+++ src/common/socialLinks.jsx
@@ -7,10 +7,10 @@ import { Github, Linkedin } from "lucide-react"
 const SocialLinks = () => {
     return (
         <nav className="flex space-x-4">
-            <a href={socialLinks.github} target="_blank" rel="noopener noreferrer" className="text-[#00d4ff]">
+            <a href={socialLinks.github} target="_blank" rel="noopener noreferrer" className="text-[#00d4ff] hover:text-white transition-colors">
                 <Github />
             </a>
-            <a href={socialLinks.linkedin} target="_blank" rel="noopener noreferrer" className=" text-[#00d4ff]" >
+            <a href={socialLinks.linkedin} target="_blank" rel="noopener noreferrer" className=" text-[#00d4ff] hover:text-white transition-colors" >
                 <Linkedin />
             </a>
         </nav>

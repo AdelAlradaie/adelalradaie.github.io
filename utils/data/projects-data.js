ccbfadf6ec9b1a036ab7eb319f376346b3f91dc1
AdelAlradaie <adelmohm209@gmail.com>
2025-06-22 01:14:11 +0300

    initial commit

---
+++ utils/data/projects-data.js
@@ -0,0 +1,29 @@
+export const projectsData = [
+    {
+        id: 1,
+        title: 'Portfolio',
+        description: "Software Developer Portfolio Website built with react.js and tailwind CSS that helps you showcase your work and skills as a software developer.",
+        techs: ['HTML','Css','React','ViteJS', 'Tailwind',],
+        image: "/portfolio.PNG",
+        url: "https://adelalradaie.github.io/"
+    },
+    {
+        id: 2,
+        title: 'Hujwzat',
+        description: 'built this project for a client of ( TECH.SOF ). ( React js - Laravel - TailwindCSS ).',
+        techs: ['HTML','Css','React','Typescript','ViteJS', 'Tailwind'],
+        image: "/hujwzat.JPG",
+        url: 'https://www.hujwzat.com/',
+    },
+    {
+        id: 3,
+        title: 'Hujwzat-Vendor',
+        description: 'built this project for a client of ( TECH.SOF ). ( React js - Laravel - TailwindCSS ).',
+        techs: ['HTML','Css','React','Typescript','ViteJS', 'Tailwind'],
+        image: "/vendor.JPG",
+        url: 'https://vendor.hujwzat.com/',
+    },
+
+];
+
+

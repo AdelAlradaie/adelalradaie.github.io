07382a4727caddbc9f35ac127ddcbaac2c646bd8
AdelAlradaie <adelmohm209@gmail.com>
2025-09-03 00:06:32 +0300

    update skill images import

--- utils/skill-image.js
+++ utils/skill-image.js
@@ -1,13 +1,8 @@
-import bootstrap from '../src/assets/svg/skills/bootstrap.svg';
 import css from '../src/assets/svg/skills/css.svg';
-import docker from '../src/assets/svg/skills/docker.svg';
-import firebase from '../src/assets/svg/skills/firebase.svg';
 import html from '../src/assets/svg/skills/html.svg';
 import javascript from '../src/assets/svg/skills/javascript.svg';
 import mongoDB from '../src/assets/svg/skills/mongoDB.svg';
-import mysql from '../src/assets/svg/skills/mysql.svg';
 import nextJS from '../src/assets/svg/skills/nextJS.svg';
-import php from '../src/assets/svg/skills/php.svg';
 import react from '../src/assets/svg/skills/react.svg';
 import tailwind from '../src/assets/svg/skills/tailwind.svg';
 import typescript from '../src/assets/svg/skills/typescript.svg';
@@ -18,8 +13,6 @@ export const skillsImage = (skill) => {
   switch (skillID) {
     case 'html':
       return html;
-    case 'docker':
-      return docker;
     case 'css':
       return css;
     case 'javascript':
@@ -30,20 +23,12 @@ export const skillsImage = (skill) => {
       return react;
     case 'typescript':
       return typescript;
-    case 'bootstrap':
-      return bootstrap;
     case 'mongodb':
       return mongoDB;
-    case 'mysql':
-      return mysql;
     case 'tailwind':
       return tailwind;
     case 'vitejs':
       return vitejs;
-    case 'php':
-      return php;
-    case 'firebase':
-      return firebase;
     case 'git':
       return git;
     default:

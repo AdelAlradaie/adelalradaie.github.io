5235095d9c269f89b422c7ffa158159847f830c9
AdelAlradaie <adelmohm209@gmail.com>
2025-12-08 23:56:39 +0300

    update file

--- src/components/homepage/contact/Contact.jsx
+++ src/components/homepage/contact/Contact.jsx
@@ -11,13 +11,13 @@ function ContactSection() {
       <div className="flex flex-col gap-5 lg:gap-">
         <h2 className='text-xl md:text-3xl'>Reach Out to me!</h2>
         <p className='text-[#d3d8e8] text-lg md:text-xl'>Discuss a project or just want to say hi? My Inbox is open for all.</p>
-        <p className="text-sm md:text-xl flex items-center gap-3">
+        {/* <p className="text-sm md:text-xl flex items-center gap-3">
           <MdAlternateEmail
             className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#1da1f2] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
             size={36}
           />
           <span>{personalData.email}</span>
-        </p>
+        </p> */}
 
       </div>
       <div className="mt-8 lg:mt-16 flex items-center gap-5 lg:gap-10">

7751dc404ff2261bcfc40e6bd924df9197ca1350
AdelAlradaie <adelmohm209@gmail.com>
2025-12-08 21:59:33 +0300

    update data

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,7 +5,7 @@ export const personalData = {
   description: `
    I'm Full Stack Developer with over 2  years of experience in software engineering and developing new features and apps for different products and clients by using programming tools like Reactjs, React-Native, Laravel, Node.js ..etc . Capable of analyzing customer feedback in order to find the best way to create new and enhance the existing product features and performing quality assurance.
   `,
-  email: 'adelalradaie@gmail.com',
+  email: 'devadelalradaie@gmail.com',
   github: 'https://github.com/AdelAlradaie',
   linkedIn: 'https://www.linkedin.com/in/adel-alradaie',
   resume: "https://drive.google.com/file/d/1Q9zioskcP3mbTrdXrtdzQx-eM3EDsW9f/view?usp=drive_link"
